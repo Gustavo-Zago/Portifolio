5687b85fc2e1114ae0592383e2e47c97e3669a23
Gustavo-Zago <rrgusta@gmail.com>
2025-05-15 22:38:58 -0300

    feat: atualização da pagina sobre, animaçoes e informações

--- sobre.js
+++ sobre.js
@@ -11,11 +11,41 @@ function revelarAoScroll_divisao() {
     }
   });
 }
+const frase = document.querySelectorAll('.frase');
+
+function revelarAoScroll_frase() {
+  const alturaJanela = window.innerHeight;
+  frase.forEach((frase) => {
+    const topo = frase.getBoundingClientRect().top;
+    if (topo < alturaJanela - 100) {
+      frase.classList.add('visivel');
+    } else {
+      frase.classList.remove('visivel');
+    }
+  });
+}
+const titulo = document.querySelectorAll('.titulo');
+
+function revelarAoScroll_titulo() {
+  const alturaJanela = window.innerHeight;
+  titulo.forEach((titulo) => {
+    const topo = titulo.getBoundingClientRect().top;
+    if (topo < alturaJanela - 100) {
+      titulo.classList.add('visivel');
+    } else {
+      titulo.classList.remove('visivel');
+    }
+  });
+}
 window.addEventListener('scroll', () => {
   revelarAoScroll_divisao();
+  revelarAoScroll_frase();
+  revelarAoScroll_titulo();
 });
 
 window.addEventListener('load', () => {
   revelarAoScroll_divisao();
+  revelarAoScroll_frase();
+  revelarAoScroll_titulo();
 
 });
